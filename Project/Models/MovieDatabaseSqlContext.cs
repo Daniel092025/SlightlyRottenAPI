aeb46bb125c365ae55bc0d5b7b5727890b1bcd83
Daniel092025 <danbo092025@gmail.com>
2025-12-10 10:41:09 +0100

    Lagt til ReviewService

--- Project/Models/MovieDatabaseSqlContext.cs
+++ Project/Models/MovieDatabaseSqlContext.cs
@@ -15,6 +15,7 @@ public partial class MovieDatabaseSqlContext : DbContext
     {
     }
 
+    public DbSet<Review> Reviews { get; set; }
     public virtual DbSet<Movie> Movies { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- Project/Services/ReviewService.cs
+++ Project/Services/ReviewService.cs
@@ -1,6 +1,34 @@
-public class ReviewService
+using Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ReviewService : IReviewService
 {
-    
+    private readonly MovieDatabaseSqlContext _context;
+    public ReviewService(MovieDatabaseSqlContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Review> AddReviewAsync(Review review)
+    {
+        _context.Reviews.Add(review);
+        await _context.SaveChangesAsync();
+        return review;
+    }
+    public async Task<Review?> UpdateReviewAsync (int reviewId, Review updated)
+    {
+        var existingReview = await _context.Reviews.FindAsync(reviewId);
+
+        if (existingReview == null)
+            return null;
+
+        existingReview.Rating = updated.Rating;
+        existingReview.Comment = updated.Comment;
+        existingReview.UserId = updated.UserId;
+
+        await _context.SaveChangesAsync();
+        return existingReview;
+    }
 }
 
 
