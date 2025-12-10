037c5cb9e77c271e1c5f40a65d0488d6719c6cac
brage092025 <brage092025@gmail.com>
2025-12-10 09:14:57 +0100

    Added dotnet ef, added new Movie.cs scaffolded from MovieDatabaseSQL

--- Project/Models/Movie.cs
+++ Project/Models/Movie.cs
@@ -1,13 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace Project.Models;
 
-public class Movie
+// Scaffolded(reverse engineered) using EF Core from MovieDatabaseSQL 
+public partial class Movie
 {
-    public string? Id {get; set; }
-    public string? Title {get; set; }
-    public string? Genre {get; set; }
-    public string? Director {get; set; }
-    public string? Actors {get; set; }
-    public int Year {get; set; }
-    public int Playtime {get; set; }
-     public int? MovieId { get; set; }
+    public string? Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Genre { get; set; }
+
+    public string? Director { get; set; }
+
+    public string? Actors { get; set; }
+
+    public int? Year { get; set; }
+
+    public int? Playtime { get; set; }
+
+    public int? Rating { get; set; }
+
+    public string? Review { get; set; }
 }
---
+++ Project/Models/MovieOld.cs
@@ -0,0 +1,13 @@
+
+
+public class Movie
+{
+    public string? Id {get; set; }
+    public string? Title {get; set; }
+    public string? Genre {get; set; }
+    public string? Director {get; set; }
+    public string? Actors {get; set; }
+    public int Year {get; set; }
+    public int Playtime {get; set; }
+     public int? MovieId { get; set; }
+}
